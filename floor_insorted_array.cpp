9325bdfb3a95d24d963237bb106c816a2c16308a
aj1063 <60145398+aj1063@users.noreply.github.com>
2022-06-06 01:13:06 +0530

    Add files via upload

---
+++ floor_insorted_array.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)cin>>a[i];
+    int res=-1;
+    int x;
+    cin>>x;
+    int l=0;
+    int end=n-1;
+    while(l<=end)
+    {
+        int mid=l+(end-l)/2;
+        if(a[mid]==x)return a[mid];
+        
+        else if(a[mid]<x)
+        {
+            res=a[mid];
+            l=mid+1;
+        }
+        else
+            end=mid-1;
+        
+    }
+
+    cout<<res;
+    return 0;
+}
+
